989eec698ea525563e369452ef2b82def4468c80
ItoNaoya <itonaoya@ItoNaoya-no-MacBook-Pro.local>
2020-02-09 13:55:23 +0900

    fix deploy setting

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,6 +8,7 @@ set :branch, 'master'
 set :deploy_to, '/var/www/web_game/'
 
 set :linked_files, fetch(:linked_files, []).push('config/settings.yml')
+set :linked_files, fetch(:linked_files, []).push("config/master.key")
 
 # シンボリックリンクをはるフォルダ。(※後述)
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
